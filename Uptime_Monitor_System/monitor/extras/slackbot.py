16077d04cb48cefb3f19ddfe84bc4aca6685e74a
Rheeder <rhdr0011@gmail.com>
2021-09-22 20:03:37 +0200

    updated slackbot: can now see last_error

--- Uptime_Monitor_System/monitor/extras/slackbot.py
+++ Uptime_Monitor_System/monitor/extras/slackbot.py
@@ -8,24 +8,27 @@ class SlackBot():
         self.__slack_token = slack_token
         self.channel = channel
         self.client = WebClient(token=slack_token)
+        self.last_error = None
 
     def post_message(self, message: str) -> bool:
         '''Post a message to slack to a hardcoded channel'''
         try:
             response = self.client.chat_postMessage(channel=self.channel,
                                                     text=message)
+            self.last_error = None
             return True
         except SlackApiError as e:
             assert e.response[
                 "error"]  # str like 'invalid_auth', 'channel_not_found'
+            self.last_error = e.response    
             return False
 
 
 if __name__ == "__main__":
     bot = SlackBot(
-        slack_token="xoxb-2503398942439-2525293310900-87ve4GJrP2hkmaTcHhuopgvG",
+        slack_token="xoxb-2503398942439-2525293310900-nKOza6roLlr4PaU0EkjoLKK9",
         channel="#uptime-monitor-system")
     if bot.post_message(message='OOP class message again!'):
         print("Message sent!")
     else:
-        print("Message failed")
+        print("Message failed", bot.last_error)
