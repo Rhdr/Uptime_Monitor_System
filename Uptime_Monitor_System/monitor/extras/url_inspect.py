6bb643f4157b083d21d91dd936cafe5f637cdaf5
Rheeder <rhdr0011@gmail.com>
2021-09-23 12:38:30 +0200

    created a ScheduledURLInspector

--- Uptime_Monitor_System/monitor/extras/url_inspect.py
+++ Uptime_Monitor_System/monitor/extras/url_inspect.py
@@ -19,6 +19,35 @@ class URLInspectTool():
         return retdict
 
 
+class ScheduledURLInspector:
+    def __init__(self, url_list: list, job_scheduler: JobScheduler,
+                 slackbot: SlackBot) -> None:
+        self.url_list = url_list
+        self.job_scheduler = job_scheduler
+        self.slackbot = slackbot
+        self.prev_inspection_job_report = {}
+
+    def start_scheduled_inspection(self) -> None:
+        self.job_scheduler.start(5,
+                                 self._inspection_job,
+                                 url_list=self.url_list)
+
+    def _inspection_job(self, url_list) -> None:
+        new_inspection_job_report = (
+            URLInspectTool.urlopen_http_response(url_list))
+        if self.prev_inspection_job_report != new_inspection_job_report:
+            print("status changed detected!", new_inspection_job_report)
+        else:
+            print("all is well, no status changed detected")
+        self.prev_inspection_job_report = new_inspection_job_report
+
+    def _slack_chat(self) -> None:
+        pass
+
+    def _db_update(self) -> None:
+        pass
+
+
 if __name__ == "__main__":
     #url_inspect_tool = URLInspectTool()
     urls = [
@@ -27,3 +56,12 @@ if __name__ == "__main__":
         "https://www.stackoverflow.com",
     ]
     print(URLInspectTool.urlopen_http_response(urls))
+
+    print('')
+    print('ScheduledURLInspector')
+
+    job_scheduler = JobScheduler()
+    slackbot = SlackBot()
+
+    sinspector = ScheduledURLInspector(urls, job_scheduler, slackbot)
+    sinspector.start_scheduled_inspection()
