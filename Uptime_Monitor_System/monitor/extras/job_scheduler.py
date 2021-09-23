d84bc478dd28f46c7fbef745d49a45cf36a7a930
Rheeder <rhdr0011@gmail.com>
2021-09-23 12:29:53 +0200

    renamed functions->jobs

--- Uptime_Monitor_System/monitor/extras/job_scheduler.py
+++ Uptime_Monitor_System/monitor/extras/job_scheduler.py
@@ -5,27 +5,26 @@ class JobScheduler:
     """A threaded scheduler that can perform a predifined function/job. 
     use .start() to start a infinte recursive job"""
     def __init__(self) -> None:
-        self.__lock = threading.Lock()
-        self.__started = False
+        self._lock = threading.Lock()
+        self._started = False
 
-    def start(self, seconds, function_to_run,
-              **function_to_run_kwargs) -> None:
+    def start(self, seconds: int, job, **job_kwargs) -> None:
         '''run the fucntion/job every x seconds'''
-        if not self.__started:
-            self.__started = True
+        if not self._started:
+            self._started = True
             self.seconds = seconds
-            self.function_to_run = function_to_run
-            self.function_to_run_kwargs = function_to_run_kwargs
-            threading.Thread(target=self.__scheduled_worker).start()
+            self.job = job
+            self.job_kwargs = job_kwargs
+            threading.Thread(target=self._scheduled_worker).start()
         else:
             raise InterruptedError('You can only start the JobScheduler once')
 
-    def __scheduled_worker(self) -> None:
+    def _scheduled_worker(self) -> None:
         '''define a recursive job'''
-        with self.__lock:
+        with self._lock:
             # print(f"{datetime.datetime.now()}: Worker: I'm working...")
-            self.function_to_run(**self.function_to_run_kwargs)
-            threading.Timer(self.seconds, self.__scheduled_worker).start()
+            self.job(**self.job_kwargs)
+            threading.Timer(self.seconds, self._scheduled_worker).start()
 
 
 if __name__ == '__main__':
@@ -36,11 +35,11 @@ if __name__ == '__main__':
         print(f"{datetime.datetime.now()}: {message}")
 
     scheduler = JobScheduler()
-    scheduler.start(1,
+    scheduler.start(3,
                     job_that_needs_doing,
                     message="Someone is doing the job!")
     print("__main__: After the worker started: I can still run!")
-    time.sleep(3)
+    time.sleep(7)
     print("__main__: After the worker started: and run some more!")
     print("__main__: After the worker started: Lets run the worker again!")
     scheduler.start(10, job_that_needs_doing)
