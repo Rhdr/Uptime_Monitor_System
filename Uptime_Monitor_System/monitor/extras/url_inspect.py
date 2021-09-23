b83b9a90bcc56e6d7001d5f77e658de54953eb07
Rheeder <rhdr0011@gmail.com>
2021-09-23 11:57:52 +0200

    updated url tool &  inspect url list ability

---
+++ Uptime_Monitor_System/monitor/extras/url_inspect.py
@@ -0,0 +1,29 @@
+import urllib.request
+from job_scheduler import JobScheduler
+from slackbot import SlackBot
+
+
+class URLInspectTool():
+    """Check a url's status with the is_alive static command"""
+    @staticmethod
+    def urlopen_http_response(url_list: list) -> dict:
+        '''Return true if a website is up and running'''
+
+        retdict = {}
+        for url in url_list:
+            try:
+                http_response_code = (urllib.request.urlopen(url).getcode())
+            except urllib.request.URLError:
+                http_response_code = 404
+            retdict[url] = http_response_code
+        return retdict
+
+
+if __name__ == "__main__":
+    #url_inspect_tool = URLInspectTool()
+    urls = [
+        "https://www.google.com/",
+        "https://www.foo.bar.com/",
+        "https://www.stackoverflow.com",
+    ]
+    print(URLInspectTool.urlopen_http_response(urls))
