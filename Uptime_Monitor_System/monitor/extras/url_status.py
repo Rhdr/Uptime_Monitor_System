b57a88500df8f4ae4894f1a2dfe6872c1f8749e0
Rheeder <rhdr0011@gmail.com>
2021-09-23 09:22:27 +0200

    updated URLStatus to store a last http response

--- Uptime_Monitor_System/monitor/extras/url_status.py
+++ Uptime_Monitor_System/monitor/extras/url_status.py
@@ -3,23 +3,31 @@ import urllib.request
 
 class URLStatus():
     """Check a url's status with the is_alive static command"""
-    @staticmethod
-    def is_alive(url: str) -> bool:
+    def __init__(self) -> None:
+        self.last_http_response = None
+
+    def is_alive(self, url: str) -> bool:
         '''Return true if a website is up and running'''
         try:
             #retval = (urllib.request.urlopen("http://foo.example.org/").getcode())
-            http_retval = (urllib.request.urlopen(url).getcode())
-            if http_retval == 200:
+            self.last_http_response = (urllib.request.urlopen(url).getcode())
+            if self.last_http_response == 200:
                 return True
             else:
                 return False
         except:
-            http_retval = 404
+            self.last_http_response = 404
             return False
 
 
 if __name__ == "__main__":
-    status = URLStatus.is_alive("https://www.stackoverflow.com")
+    url_status = URLStatus()
+    status = url_status.is_alive("https://www.stackoverflow.com")
     print(f'https://www.stackoverflow.com: {status}')
-    status = URLStatus.is_alive("http://foo.example.org/")
+    status = url_status.is_alive("http://foo.example.org/")
     print(f'http://foo.example.org/: {status}')
+    status = url_status.is_alive("https://www.google.com/")
+    print(f'https://www.google.com: {status}')
+    print(url_status.last_http_response)
+    print(f'neverssl.com/: {status}')
+    print(url_status.last_http_response)
