9fa7706ba1e6e094259da39db35b0e850d620d7d
Rheeder <rhdr0011@gmail.com>
2021-09-23 16:59:35 +0200

    created site model

--- Uptime_Monitor_System/monitor/models.py
+++ Uptime_Monitor_System/monitor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+#models
+class Sites(models.Model):
+    pk_sites = models.AutoField(primary_key=True)
+    site_name = models.CharField(max_length=264, unique=True)
+    site_url = models.URLField(unique=True)
+    site_webhook = models.URLField(unique=True)
+    site_status = models.BooleanField()
+    slack_account_url = models.URLField()
+
+    def __str__(self):
+        return f'{self.name}'
