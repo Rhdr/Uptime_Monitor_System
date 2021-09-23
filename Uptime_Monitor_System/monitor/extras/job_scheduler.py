d18ccc423e1e26d8e370273db32ad245ef5e70cc
Rheeder <rhdr0011@gmail.com>
2021-09-23 12:11:34 +0200

    JobScheduler can now pass kwargs to jobs functions

--- Uptime_Monitor_System/monitor/extras/job_scheduler.py
+++ Uptime_Monitor_System/monitor/extras/job_scheduler.py
@@ -8,12 +8,14 @@ class JobScheduler:
         self.__lock = threading.Lock()
         self.__started = False
 
-    def start(self, seconds, function_to_run) -> None:
+    def start(self, seconds, function_to_run,
+              **function_to_run_kwargs) -> None:
         '''run the fucntion/job every x seconds'''
         if not self.__started:
             self.__started = True
             self.seconds = seconds
             self.function_to_run = function_to_run
+            self.function_to_run_kwargs = function_to_run_kwargs
             threading.Thread(target=self.__scheduled_worker).start()
         else:
             raise InterruptedError('You can only start the JobScheduler once')
@@ -22,7 +24,7 @@ class JobScheduler:
         '''define a recursive job'''
         with self.__lock:
             # print(f"{datetime.datetime.now()}: Worker: I'm working...")
-            self.function_to_run()
+            self.function_to_run(**self.function_to_run_kwargs)
             threading.Timer(self.seconds, self.__scheduled_worker).start()
 
 
@@ -30,11 +32,13 @@ if __name__ == '__main__':
     import datetime
     import time
 
-    def job_that_needs_doing():
-        print(f"{datetime.datetime.now()}: Someone is doing the job!")
+    def job_that_needs_doing(message='working'):
+        print(f"{datetime.datetime.now()}: {message}")
 
     scheduler = JobScheduler()
-    scheduler.start(1, job_that_needs_doing)
+    scheduler.start(1,
+                    job_that_needs_doing,
+                    message="Someone is doing the job!")
     print("__main__: After the worker started: I can still run!")
     time.sleep(3)
     print("__main__: After the worker started: and run some more!")
