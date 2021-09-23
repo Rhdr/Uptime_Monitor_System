705fe00496d006a4c35a11ce87e786d41a229de4
Rheeder <rhdr0011@gmail.com>
2021-09-23 21:40:20 +0200

    updated template/view to show model data

--- Uptime_Monitor_System/monitor/views.py
+++ Uptime_Monitor_System/monitor/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Website
+
 
 # Views
 def home(request):
-    return render(request, 'monitor/home.html')
+    websites = Website.objects.order_by('site_name')
+
+    #add row number
+    for row_number, website in enumerate(websites):
+        website.row_number = row_number + 1
+        print(website, website.row_number)
+    context_dict = {'websites': websites}
+
+    return render(request, 'monitor/home.html', context_dict)
