3a72a3aec003183f30b6446b616b162fcb01914b
Rheeder <rhdr0011@gmail.com>
2021-09-24 10:07:27 +0200

    removed seperate webhookurl & aded http response

--- Uptime_Monitor_System/monitor/models.py
+++ Uptime_Monitor_System/monitor/models.py
@@ -6,7 +6,7 @@ class Website(models.Model):
     pk_website = models.AutoField(primary_key=True)
     site_name = models.CharField(max_length=264, unique=True)
     site_url = models.URLField(unique=True)
-    site_webhook = models.URLField(unique=True)
+    site_last_http_response = models.IntegerField()
     site_status = models.BooleanField()
     slack_account_url = models.URLField()
 
