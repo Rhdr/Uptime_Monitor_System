c2b0bc21478a4b72aceb2eed92db6278dcd32d89
Rheeder <rhdr0011@gmail.com>
2021-09-25 00:09:08 +0200

    Added add form

---
+++ Uptime_Monitor_System/monitor/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.db.models import fields
+from .models import Website
+
+
+class WebsiteAddUpdateForm(forms.ModelForm):
+    # site_name = forms.CharField()
+    # site_url = forms.URLField()
+    # slack_account_url = forms.URLField()
+
+    class Meta:
+        model = Website
+        fields = ("site_name", "site_url", "slack_account_url")
--- Uptime_Monitor_System/monitor/views.py
+++ Uptime_Monitor_System/monitor/views.py
@@ -2,22 +2,32 @@ from django.shortcuts import render
 from .extras.job_scheduler import JobScheduler
 from .extras.slackbot import SlackBot
 from .extras.url_inspect import Inspector
+from .forms import WebsiteAddUpdateForm
 from .models import Website
 
 
 # Views
 def home(request):
-    # start website inspections & update db
+    # # start website inspections & update db
     job_scheduler = JobScheduler()
     #slackbot = SlackBot()
     slackbot = None
     inspector = Inspector(job_scheduler, slackbot)
     inspector.start_scheduled_inspection()
 
+    form = WebsiteAddUpdateForm()
+
     #add row number
     websites = Website.objects.order_by('site_name')
     for row_number, website in enumerate(websites):
         website.row_number = row_number + 1
-    context_dict = {'websites': websites}
+
+    context_dict = {'websites': websites, 'form': form}
+
+    if request.method == 'POST':
+        form = WebsiteAddUpdateForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            #print("SiteName", form.cleaned_data['site_name'])
 
     return render(request, 'monitor/home.html', context_dict)
