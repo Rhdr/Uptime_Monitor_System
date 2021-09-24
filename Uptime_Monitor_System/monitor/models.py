e397139a5fa04a85ac1b27f6c4d4d35e84e38d9c
Rheeder <rhdr0011@gmail.com>
2021-09-24 23:48:36 +0200

    added default values to db

--- Uptime_Monitor_System/monitor/models.py
+++ Uptime_Monitor_System/monitor/models.py
@@ -6,9 +6,9 @@ class Website(models.Model):
     pk_website = models.AutoField(primary_key=True)
     site_name = models.CharField(max_length=264, unique=True)
     site_url = models.URLField(unique=True)
-    site_last_http_response = models.IntegerField()
-    site_status = models.BooleanField()
-    slack_account_url = models.URLField()
+    site_last_http_response = models.IntegerField(default=404)
+    site_up_status = models.BooleanField(default=False)
+    slack_account_url = models.URLField(default='https://www.rheeder.slack.com')
 
     def __str__(self):
         return f'{self.site_name}'
