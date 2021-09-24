6127f8753d77d163e571fa07aef04afb91937f18
Rheeder <rhdr0011@gmail.com>
2021-09-24 22:16:19 +0200

    updated template to call inspector

--- Uptime_Monitor_System/monitor/views.py
+++ Uptime_Monitor_System/monitor/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+from .extras.job_scheduler import JobScheduler
+from .extras.slackbot import SlackBot
+from .extras.url_inspect import Inspector
 from .models import Website
 
 
 # Views
 def home(request):
-    websites = Website.objects.order_by('site_name')
+    # start website inspections & update db
+    job_scheduler = JobScheduler()
+    #slackbot = SlackBot()
+    slackbot = None
+    inspector = Inspector(job_scheduler, slackbot)
+    inspector.start_scheduled_inspection()
 
     #add row number
+    websites = Website.objects.order_by('site_name')
     for row_number, website in enumerate(websites):
         website.row_number = row_number + 1
-        print(website, website.row_number)
     context_dict = {'websites': websites}
 
     return render(request, 'monitor/home.html', context_dict)
