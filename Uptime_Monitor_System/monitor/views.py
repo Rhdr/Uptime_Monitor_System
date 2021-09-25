6da4b779c7e3ba491a78a776e43afa4b80b8ac67
Rheeder <rhdr0011@gmail.com>
2021-09-25 10:06:07 +0200

    added new modals

--- Uptime_Monitor_System/monitor/views.py
+++ Uptime_Monitor_System/monitor/views.py
@@ -8,26 +8,31 @@ from .models import Website
 
 # Views
 def home(request):
-    # # start website inspections & update db
-    job_scheduler = JobScheduler()
-    #slackbot = SlackBot()
-    slackbot = None
-    inspector = Inspector(job_scheduler, slackbot)
-    inspector.start_scheduled_inspection()
+    # # # start website inspections & update db
+    # job_scheduler = JobScheduler()
+    # #slackbot = SlackBot()
+    # slackbot = None
+    # inspector = Inspector(job_scheduler, slackbot)
+    # inspector.start_scheduled_inspection()
 
-    form = WebsiteAddUpdateForm()
+    update_form = WebsiteAddUpdateForm()
 
     #add row number
     websites = Website.objects.order_by('site_name')
     for row_number, website in enumerate(websites):
         website.row_number = row_number + 1
 
-    context_dict = {'websites': websites, 'form': form}
-
     if request.method == 'POST':
-        form = WebsiteAddUpdateForm(request.POST)
-        if form.is_valid():
-            form.save(commit=True)
-            #print("SiteName", form.cleaned_data['site_name'])
+        add_form = WebsiteAddUpdateForm(request.POST)
+        print("SiteName", add_form.cleaned_data['site_name'])
+        if add_form.is_valid():
+            add_form.save(commit=True)
+    else:
+        add_form = WebsiteAddUpdateForm()
 
+    context_dict = {
+        'websites': websites,
+        'add_form': add_form,
+        'update_form': update_form
+    }
     return render(request, 'monitor/home.html', context_dict)
