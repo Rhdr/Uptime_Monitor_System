082b047e967e7dd5174a57fab5fa796c16bff76c
Rheeder <rhdr0011@gmail.com>
2021-09-25 22:15:29 +0200

    added js

--- Uptime_Monitor_System/Uptime_Monitor_System/settings.py
+++ Uptime_Monitor_System/Uptime_Monitor_System/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    STATIC_DIR,
+    BASE_DIR / "static",
 ]
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
