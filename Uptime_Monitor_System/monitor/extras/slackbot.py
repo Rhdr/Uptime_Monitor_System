255e7c3f02d77a7381fbec50bd976ec91133c2a6
Rheeder <rhdr0011@gmail.com>
2021-09-26 11:58:41 +0200

    removed old slackbot

--- Uptime_Monitor_System/monitor/extras/slackbot.py
+++ Uptime_Monitor_System/monitor/extras/slackbot.py
@@ -6,30 +6,6 @@ from dotenv import load_dotenv
 import requests
 
 
-class SlackBot_old():
-    """Slack messagin bot - Every instance is tied to a channel. Create a slack_token and a channel then use it to create a SlackBot"""
-    def __init__(self, channel="#uptime-monitor-system") -> None:
-        dotenv_path = join(dirname(__file__), '.env')
-        load_dotenv(dotenv_path)
-        token = os.environ.get('SLACK_TOKEN')
-        self._client = WebClient(token)
-        self.channel = "#uptime-monitor-system"
-        self.last_error = None
-
-    def post_message(self, message: str) -> bool:
-        '''Post a message to slack to a hardcoded channel'''
-        try:
-            response = self._client.chat_postMessage(channel=self.channel,
-                                                     text=message)
-            self.last_error = None
-            return True
-        except SlackApiError as e:
-            assert e.response[
-                "error"]  # str like 'invalid_auth', 'channel_not_found'
-            self.last_error = e.response
-            return False
-
-
 class SlackBot():
     @staticmethod
     def post_message(slack_token: str, slack_channel: str,
