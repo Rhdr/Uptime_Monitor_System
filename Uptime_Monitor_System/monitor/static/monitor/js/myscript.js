4abc3d3e398cc3e0af9e9b5cbe9e83d89aab9c10
Rheeder <rhdr0011@gmail.com>
2021-09-27 00:51:21 +0200

    added ajax edit/add selectors

--- Uptime_Monitor_System/monitor/static/monitor/js/myscript.js
+++ Uptime_Monitor_System/monitor/static/monitor/js/myscript.js
@@ -1,49 +1,58 @@
-var a_add = document.querySelector("#a_add")
-var a_edit_tbl = document.querySelectorAll("#a_edit_tbl")
-var a_delete_tbl = document.querySelectorAll("#a_delete_tbl")
-// var a_delete_modal = document.querySelector("#a_delete_modal")
+// $(document).ready(function () {
+//     $('.btn_edit').click(function () {
+//         var btn_id = this.id; // button ID 
+//         var tr_id = $(this).closest('tr').attr('id'); // table row ID 
+//         console.log("btn_id")
+//         console.log("tr_id")
+//     });
+// });
 
-var modal_addedit = new bootstrap.Modal(document.getElementById("btn_add_site"), {});
-var modal_delete = new bootstrap.Modal(document.getElementById("btn_delete_site"), {});
-var modal_to_open = "NONE";
-// var delete_pk = -1;
+$(document).on("click", '#btn_add', function (event) {
+    $('#add_edit_modal').val('ADD')
+    $('#add_edit_modal_pk_website').val(-1)
+    $('#add_edit_modal_site_name').val('')
+    $('#add_edit_modal_site_url').val('')
+    $('#add_edit_modal_slack_token').val('')
+    $('#add_edit_modal_slack_channel').val('')
+});
 
+$(document).on("click", '.btn_edit', function (event) {
+    // console.log("edit btn clicked!")
+    //get current row & then the unique url
+    var current_row = $(this).closest("tr");
+    var cell_site_url = current_row.find("td:eq(1)").text();
+    // console.log("cell_site_url: " + cell_site_url)
 
-a_add.addEventListener("click", function () {
-    modal_to_open = "ADD_EDIT"
-    localStorage.setItem("modal_to_open_key", modal_to_open);
-    a_add.click()
-})
+    //get slack accounts & pk
+    $.ajax({
+        type: 'GET',
+        url: "ajax_json/",
+        success: function (response) {
+            for (var key in response.websites) {
+                var obj_site_url = response.websites[key].site_url
+                if (cell_site_url == obj_site_url) {
+                    pk_website = response.websites[key].pk_website
+                    site_name = response.websites[key].site_name
+                    site_url = obj_site_url
+                    slack_token = response.websites[key].slack_token
+                    slack_channel = response.websites[key].slack_channel
+                    // console.log("obj_pk_website:" + pk_website)
+                    // console.log("obj_slack_token:" + slack_token)
+                    // console.log("obj_slack_channel:" + slack_channel)
 
-a_edit_tbl.forEach(edit_list_listener);
-function edit_list_listener(a_edit) {
-    a_edit.addEventListener("click", function () {
-        modal_to_open = "ADD_EDIT"
-        localStorage.setItem("modal_to_open_key", modal_to_open);
-        a_edit.click()
-    })
-}
+                    $('#add_edit_modal').val('EDIT')
+                    $('#add_edit_modal_pk_website').val(pk_website)
+                    $('#add_edit_modal_site_name').val(site_name)
+                    $('#add_edit_modal_site_url').val(site_url)
+                    $('#add_edit_modal_slack_token').val(slack_token)
+                    $('#add_edit_modal_slack_channel').val(slack_channel)
+                    break;
+                }
+            }
+        },
+        error: function (data) {
+            console.log("An error occured");
+        }
+    });
 
-a_delete_tbl.forEach(delete_list_listener);
-function delete_list_listener(a_delete) {
-    a_delete.addEventListener("click", function () {
-        modal_to_open = "DELETE"
-        localStorage.setItem("modal_to_open_key", modal_to_open);
-        // localStorage.setItem("modal_to_open_key", modal_to_open);
-        a_delete.click()
-    })
-}
-
-window.onload = function () {
-    var modal_to_open = localStorage.getItem("modal_to_open_key");
-    console.log(modal_to_open)
-
-    if (modal_to_open == "ADD_EDIT") {
-        modal_addedit.show()
-    } else if (modal_to_open == "DELETE") {
-        modal_delete.show()
-    }
-
-    modal_to_open = "NONE"
-    localStorage.setItem("modal_to_open_key", modal_to_open)
-};
+});
