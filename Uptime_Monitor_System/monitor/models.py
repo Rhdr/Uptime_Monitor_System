deea1bc0010af2612dabb6776a579ea3aa4e15e1
Rheeder <rhdr0011@gmail.com>
2021-09-26 10:10:33 +0200

    added slack auth & chanel to db

--- Uptime_Monitor_System/monitor/models.py
+++ Uptime_Monitor_System/monitor/models.py
@@ -8,7 +8,8 @@ class Website(models.Model):
     site_url = models.URLField(unique=True)
     site_last_http_response = models.IntegerField(default=404)
     site_up_status = models.BooleanField(default=False)
-    slack_account_url = models.URLField(default='https://www.rheeder.slack.com')
+    slack_token = models.CharField(max_length=264)
+    slack_channel = models.CharField(max_length=264)
 
     def __str__(self):
         return f'{self.site_name}'
